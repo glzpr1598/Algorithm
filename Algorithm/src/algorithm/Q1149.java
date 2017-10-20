f916bf6f8d4188077fed3ba32efa60b0fb67c5b3
kehy5 <kehy5@DESKTOP-IHAHA3M>
2017-10-21 00:50:06 +0900

    1149 RGB거리, Q14503 로봇 청소기

--- Algorithm/src/algorithm/Q1149.java
+++ Algorithm/src/algorithm/Q1149.java
@@ -1,14 +1,53 @@
 package algorithm;
 
-//RGB거리
-//RGB거리에 사는 사람들은 집을 빨강, 초록, 파랑중에 하나로 칠하려고 한다. 
-//또한, 그들은 모든 이웃은 같은 색으로 칠할 수 없다는 규칙도 정했다. 
-//집 i의 이웃은 집 i-1과 집 i+1이다. 처음 집과 마지막 집은 이웃이 아니다.
-//각 집을 빨강으로 칠할 때 드는 비용, 초록으로 칠할 때 드는 비용, 
-//파랑으로 드는 비용이 주어질 때, 모든 집을 칠할 때 드는 비용의 최솟값을 구하는 프로그램을 작성하시오.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 1149 RGB거리
+// https://www.acmicpc.net/problem/1149
+// DP문제
 public class Q1149 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");	// 첫째줄 입력
+		int n = Integer.parseInt(str[0]);	// 집 수
 		
+		// 집마다의 RGB 비용
+		int[][] p = new int[n+1][3];
+		
+		// i번째 집을 j로 칠했을 때까지의 최소비용
+		int[][] dp = new int[n+1][3];
+		
+		// RGB 입력
+		for(int i=1; i<=n; i++) {
+			str = br.readLine().split(" ");
+			p[i][0] = Integer.parseInt(str[0]);
+			p[i][1] = Integer.parseInt(str[1]);
+			p[i][2] = Integer.parseInt(str[2]);
+		}
+		
+		// DP
+		for (int i=1; i<=n; i++)
+		{
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + p[i][0]; 
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + p[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + p[i][2];
+		}
+		
+		// 마지막 n번째 집에서 3가지 경우 중 최솟값
+		int ans = min(dp[n][0], dp[n][1], dp[n][2]);
+		
+		System.out.println(ans);
+		
+	}
+	
+	// 세 수중 최솟값 리턴
+	static int min(int r, int g, int b) {
+		int ans = r;
+		if (g < ans) ans = g;
+		if (b < ans) ans = b;
+		
+		return ans;
 	}
 }
 
